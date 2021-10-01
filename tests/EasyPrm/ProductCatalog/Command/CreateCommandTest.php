9fc42b79600d1e33326db2f027b4d3ded7bf37f3
Nicolas Canfrere <nico.canfrere@gmail.com>
2021-10-01 13:08:35 +0200

    doc : test Price CreateCommand and label uniqueness

--- tests/EasyPrm/ProductCatalog/Command/CreateCommandTest.php
+++ tests/EasyPrm/ProductCatalog/Command/CreateCommandTest.php
@@ -7,7 +7,11 @@ use EasyPrm\Core\Factory\IdentifierFactory;
 use EasyPrm\Core\ValueObject\Identifier;
 use EasyPrm\ProductCatalog\Command\Price\CreateCommand;
 use EasyPrm\ProductCatalog\Contract\PriceInterface;
+use EasyPrm\ProductCatalog\Exception\PriceAlreadyExistsException;
 use EasyPrm\ProductCatalog\Factory\PriceFactory;
+use EasyPrm\ProductCatalog\Price;
+use EasyPrm\ProductCatalog\ValueObject\Amount;
+use EasyPrm\ProductCatalog\ValueObject\Currency;
 use EasyPrm\Tests\ProductCatalog\Repository\InMemoryPriceRepository;
 use EasyPrm\Tests\Transliterator;
 use PHPUnit\Framework\TestCase;
@@ -40,5 +44,37 @@ class CreateCommandTest extends TestCase
         );
         $command->handle(['label' => 'label', 'amount' => 100, 'currency' => 'EUR']);
         $this->assertInstanceOf(PriceInterface::class, $repository->oneByLabel('label'));
+    }
+
+    /**
+     * @test
+     */
+    public function throwProductAlreadyExistsException()
+    {
+        $sameLabel = 'label';
+        $transliterator = new Transliterator();
+        $repository = new InMemoryPriceRepository($transliterator);
+        $price = new Price(
+            Identifier::create('a'),
+            $sameLabel,
+            $sameLabel,
+            Amount::create(100),
+            Currency::create('EUR')
+        );
+        $repository->save($price);
+        $identifierFactory = $this->createMock(IdentifierFactoryInterface::class);
+        $identifierFactory->method('create')->willReturn(Identifier::create('b'));
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $factory = new PriceFactory(
+            $identifierFactory,
+            $transliterator
+        );
+        $command = new CreateCommand(
+            $factory,
+            $repository,
+            $eventDispatcher
+        );
+        $this->expectException(PriceAlreadyExistsException::class);
+        $command->handle(['label' => 'label', 'amount' => 100, 'currency' => 'EUR']);
     }
 }
