7e15318dd268a522612ec0a30ca0b8f34b3501ef
Nicolas Canfrere <nico.canfrere@gmail.com>
2021-10-01 14:23:13 +0200

    doc : Product Factory and Command, no tests

---
+++ src/EasyPrm/ProductCatalog/Command/Product/CreateCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace EasyPrm\ProductCatalog\Command\Product;
+
+use EasyPrm\ProductCatalog\Contract\ProductFactoryInterface;
+use EasyPrm\ProductCatalog\Contract\ProductRepositoryInterface;
+use EasyPrm\ProductCatalog\Dto\ProductDto;
+use EasyPrm\ProductCatalog\Event\ProductCreatedEvent;
+use EasyPrm\ProductCatalog\Exception\ProductAlreadyExistsException;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class CreateCommand
+ */
+class CreateCommand
+{
+    /** @var ProductFactoryInterface */
+    private $productFactory;
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+    /** @var EventDispatcherInterface */
+    private $eventDispatcher;
+
+    /**
+     * CreateCommand constructor.
+     *
+     * @param ProductFactoryInterface $productFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(
+        ProductFactoryInterface $productFactory,
+        ProductRepositoryInterface $productRepository,
+        EventDispatcherInterface $eventDispatcher
+    ) {
+        $this->productFactory    = $productFactory;
+        $this->productRepository = $productRepository;
+        $this->eventDispatcher   = $eventDispatcher;
+    }
+
+    public function handle(ProductDto $dto)
+    {
+        //TODO data validation
+        $exists = $this->productRepository->oneByLabel($dto->label);
+        if ($exists) {
+            throw new ProductAlreadyExistsException();
+        }
+        $product = $this->productFactory->create($dto->label);
+        $this->productRepository->save($product);
+        $this->eventDispatcher->dispatch(
+            new ProductCreatedEvent($product)
+        );
+    }
+}
---
+++ src/EasyPrm/ProductCatalog/Dto/ProductDto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EasyPrm\ProductCatalog\Dto;
+
+/**
+ * Class ProductDto
+ */
+class ProductDto
+{
+    /**
+     * @var string|null
+     */
+    public $label;
+}
