04e724fd2650fa9de5413455e45314bd2b3baae3
Nicolas Canfrere <nico.canfrere@gmail.com>
2021-10-02 04:48:20 +0200

    doc : remove product

---
+++ src/EasyPrm/ProductCatalog/Command/Product/RemoveCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace EasyPrm\ProductCatalog\Command\Product;
+
+use EasyPrm\ProductCatalog\Contract\ProductRepositoryInterface;
+use EasyPrm\ProductCatalog\Dto\ProductDto;
+use EasyPrm\ProductCatalog\Event\ProductRemovedEvent;
+use EasyPrm\ProductCatalog\Exception\ProductNotFoundException;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class RemoveCommand
+ */
+class RemoveCommand
+{
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+    /** @var EventDispatcherInterface */
+    private $eventDispatcher;
+
+    /**
+     * RemoveCommand constructor.
+     *
+     * @param ProductRepositoryInterface $productRepository
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        EventDispatcherInterface $eventDispatcher
+    ) {
+        $this->productRepository = $productRepository;
+        $this->eventDispatcher   = $eventDispatcher;
+    }
+
+    public function handle(ProductDto $dto)
+    {
+        $product = $this->productRepository->oneByIdentifier($dto->identifier);
+        if (!$product) {
+            throw new ProductNotFoundException();
+        }
+        $this->productRepository->remove($product);
+        $this->eventDispatcher->dispatch(
+            new ProductRemovedEvent($product)
+        );
+    }
+}
